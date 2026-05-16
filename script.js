95494bc50dd94c0d2272ae147aa36040935974bf
akosiroel <gelaciolialyn1023@gmail.com>
2026-05-16 23:14:38 +0800

    test user only

--- script.js
+++ script.js
@@ -583,4 +583,22 @@
             await loadData();
             updateDashboard();
         }
+
+        async function testInsert() {
+
+    const { data, error } = await window.supabaseClient.client
+        .from('drills')
+        .insert([
+            {
+                user_email: 'lia@gmail.com',
+                subject: 'Math',
+                score: 100
+            }
+        ]);
+
+    console.log('DATA:', data);
+    console.log('ERROR:', error);
+}
+
+testInsert();
     };
